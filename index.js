41d58bcbdcdd134642341aa41e5e8b572616992b
Yomaira Escano <yjescano@gmail.com>
2018-07-24 04:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(name) {
-  capitalizedName = customerName.toUpperCase(name);
+  capitalizedName = customerName.toUpperCase.all(name);
   return capitalizedName;
 }
 upperCaseCustomerName(name)
