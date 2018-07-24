2c473f5deca978b69462af83b6552e3d95b0f117
Yomaira Escano <yjescano@gmail.com>
2018-07-24 03:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
+customerName.toUpperCase(0);
+
 function upperCaseCustomerName(customerName) {
 
   return customerName.toUpperCase();
